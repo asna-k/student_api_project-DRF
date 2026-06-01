00f2e1889c7be227985e7d775c20a778dd97980e
Asna K <asnaalikulangarath@gmail.com>
2026-06-01 12:23:36 +0530

    donee

--- student_api_project/settings.py
+++ student_api_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xfm%4q86_v$%70woqb4lq4_f1w@*!an77sc^sfovo87hdh*!9('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 REST_FRAMEWORK = {
